8840158e2ef4f9a18e563754bcd0c9afe5a11105
Yin-Hsun, Huang <thomas820605@gmail.com>
2017-02-24 01:46:57 +0800

    mm_init(), mm_alloc() in dlist

--- mm.c
+++ mm.c
@@ -64,17 +64,49 @@ team_t team = {
 
 /* Given block ptr bp, compute address of its header and footer */
 #define HDRP(bp)     ((char *)(bp) - WSIZE)
-#define FTRP(bp)     ((char *)(bp) - GET_SIZE(HDRP(bp)) - DSIZE)
+#define FTRP(bp)     ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
 /* Given block ptr bp, compute address of next and previous blocks */
 #define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+/* Caculate head_listp from mem_lo() */
+#define HEAD_LISTP    ( ((char *)mem_heap_lo() + DSIZE))
+/* private function to extend heap size*/
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+    size = words * WSIZE
+
+    /* Allocate an aligned number of words */
+    size_t newsize = ALIGN(size + SIZE_T_SIZE);
+    if((bp = mem_sbrk(size)) == (void *)-1)
+        return NULL;
+
+    /* Initialize free block header/footer and the epilogue header */
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return (void *)bp;
+}
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    char *heap_listp;
+    if((heap_listp = mem_sbrk(4*WSIZE) == (void *)-1)
+        return -1;
+    PUT(head_listp, 0);
+    PUT(head_listp + (1*WSIZE), PACK(DSIZE, 1));
+    PUT(head_listp + (2*WSIZE), PACK(DSIZE, 1));
+    PUT(head_listp + (3*WSIZE), PACK(0, 1));
+
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
     return 0;
 }
 
@@ -85,6 +117,9 @@ int mm_init(void)
 void *mm_malloc(size_t size)
 {
     int newsize = ALIGN(size + SIZE_T_SIZE);
+    char *heap_listp = HEAP_LISTP;
+
+
     void *p = mem_sbrk(newsize);
     if (p == (void *)-1)
 	return NULL;
